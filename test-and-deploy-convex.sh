29e625ffea8d8c5f7d9c6c71fc79d2869755a063
Jeffrey Skinner <jeff@je.gy>
2026-03-06 15:42:40 +0900

    Oh uhhh i euyu ijgi oh o hoja cool :)

--- test-and-deploy-convex.sh
+++ test-and-deploy-convex.sh
@@ -5,4 +5,4 @@ bash scripts/validate-code.sh
 npx tsx scripts/build-frontend.ts
 npx wrangler pages deploy dist/ --project-name="$(node --print "require('./package.json').name")" --commit-dirty=true
 bash scripts/replay-journal.sh
-npx convex deploy --yes --commit-dirty=true
+npx convex deploy --yes
