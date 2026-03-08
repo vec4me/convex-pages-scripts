9c5c6e08239c6e38597765abff62b74fa14eb6e8
Jeffrey Skinner <jeff@je.gy>
2026-03-08 15:37:33 +0900

    Whoops that was bad (the frontend was deployed using the dev database)

--- test-and-deploy-convex.sh
+++ test-and-deploy-convex.sh
@@ -2,7 +2,6 @@ set -o errexit -o nounset -o pipefail -o noclobber
 
 npx convex dev --once --typecheck=enable
 bash scripts/validate-code.sh
-npx tsx scripts/build-frontend.ts
-npx wrangler pages deploy dist/ --project-name="$(node --print "require('./package.json').name")" --commit-dirty=true
 bash scripts/replay-journal.sh
-npx convex deploy --yes
+npx convex deploy --yes --cmd="npx tsx scripts/build-frontend.ts" --cmd-url-env-var-name=CONVEX_URL
+npx wrangler pages deploy dist/ --project-name="$(node --print "require('./package.json').name")" --commit-dirty=true
